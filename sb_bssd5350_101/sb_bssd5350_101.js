5bbcc4f6dffce20d9e74a00f9b41e756140ee007
scottbing <bingscott@gmail.com>
2022-03-22 07:53:11 -0600

    Add files via upload

---
+++ sb_bssd5350_101/sb_bssd5350_101.js
@@ -0,0 +1,129 @@
+let LARGE_SCALES = []
+let SMALL_SCALES = []
+let img;
+function preload() {
+  img = loadImage('assests/wms.jpg')
+}
+
+function setup() {
+  
+  //90 percent of the time we will choose smaller values
+  for(let i=0; i<90; i++){
+     LARGE_SCALES.push(10)
+     LARGE_SCALES.push(20) 
+     SMALL_SCALES.push(1) 
+     SMALL_SCALES.push(2)
+  }
+  //10 percent we will choose larger values
+  for(let i=0; i<10; i++){ 
+     SMALL_SCALES.push(5) 
+     SMALL_SCALES.push(7) 
+     LARGE_SCALES.push(50) 
+     LARGE_SCALES.push(70)
+  }
+
+  img.loadPixels()
+  //print things like this
+  //console.log(img.width, img.height)
+  //create a canvas as big as your image
+  createCanvas(img.width, img.height)
+  
+  let pixelSize = 2
+  
+  let SCALES = SMALL_SCALES
+  //round will push numbers to be 1 half the time
+  let randomChoice = round(random(0,1))
+  // change from the defualt if we made the other choice
+  if(randomChoice ==1) {
+    SCALES = LARGE_SCALES
+  }
+
+  let randomColor = round(random(1, 100))
+  //1 percent chance of choosing this
+  let negativePallette = false
+  if(randomColor == 1) {
+    console.log(negativePallette)
+    negativePalette = true
+  }
+  
+  allRandom(pixelSize)
+  //firstPass(pixelSize, SCALES)
+  //secondPass(12, pixelSize) 
+}
+
+function allRandom(pixelSize, SCALES) {
+  rectMode(CENTER)
+  createCanvas(img.width, img.height)
+  for (x = 0; x < img.width; x+=pixelSize) {
+    for (y = 0; y < img.height; y+=pixelSize) { 
+      index = (floor(x) + floor(y) * img.width) * 4;
+      r = img.pixels[index]
+      g = img.pixels[index + 1] //he switched g and b
+      b = img.pixels[index + 2]
+      a = img.pixels[index + 3]
+      //pixel_brightness = (red + blue + green) / 3
+      noStroke(); //black stroke hides 1px box color
+      fill(r, g, b, a) //should be rgba
+      
+      let randSz = random(SCALES) * pixelSize
+      drawRect(randSz)
+    }
+  }
+}
+
+
+function drawRect(randSz){
+  push()
+  translate(x,y)
+  scale(randSz)
+  rotate(radians(random(10,25)))
+  rect(0, 0, 1, 1)
+  pop()
+}
+
+
+function secondPass(pixelAmount, pixelSize) {
+  for(let p = 0; p<pixelAmount; p++) {
+    //choose random x and y to draw big square. 
+    let x = Math.floor(random(img.width));
+    let y = Math.floor(random(img.height));
+
+    //rest of code is mostly copy paste from firstpass
+    index = (floor(x) + floor(y) * img.width) * 4;
+    r = img.pixels[index]
+    g = img.pixels[index + 1] //he switched g and b
+    b = img.pixels[index + 2] 
+    a = img.pixels[index + 3]
+
+    noStroke(); //black stroke hides 1px box color 
+    fill(r, g, b, a) //should be rgba
+
+    let randSz = random(3,4) * pixelSize; 
+    drawRect(randSz)
+  }
+}
+
+function firstPass(pixelSize, SCALES, negativePallette) { 
+  rectMode(CENTER)
+  createCanvas(img.width, img.height)
+  for (x = 0; x < img.width; x+=pixelSize) {
+    for (y = 0; y < img.height; y+=pixelSize) { 
+      index = (floor(x) + floor(y) * img.width) * 4;
+      r = img.pixels[index]
+      g = img.pixels[index + 1] //he switched g and b
+      b = img.pixels[index + 2]
+      a = img.pixels[index + 3]
+      if (negativePallette) {
+        r = (255-r)%255
+        g = (255-g)%255
+        b = (255-b)%255
+      }
+      //pixel_brightness = (red + blue + green) / 3
+      noStroke(); //black stroke hides 1px box color
+      fill(r, g, b, a) //should be rgba
+      
+      let randSz = random(SCALES) * pixelSize
+      drawRect(randSz)
+    }
+  }
+}
